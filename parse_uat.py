e044c3502a77b2b527f1f6fad6f2f3501bb9802f
sleeptightAnsiC <91839286+sleeptightAnsiC@users.noreply.github.com>
2024-03-16 22:39:31 +0100

    remove abstraction for string literals + few visual code changes

--- parse_uat.py
+++ parse_uat.py
@@ -4,27 +4,28 @@
 #
 # Following script invokes UnrealAutomationTool twice
 #   and parses the output so we can get every UAT command, help,
-#   parameter, default argument and parameter description
+#   parameter, parameter's description and default arguments
 # After parsing is done, the result is saved to 'out_commands' variable
 ##
+
+# WARN: we are using ue4cli for getting the RunUAT script
 
 import subprocess
 import dataclasses
 import pickle
 
 # raw output of RunUAT -List
-# WARN: we are using ue4cli for getting the RunUAT script
 proc_list = subprocess.run(["ue4", "uat", "-List", "-utf8output"], capture_output=True, text=True)
 assert proc_list.returncode == 0
 
 # convert to: RunUAT Command1 -Help Command2 -Help ... CommandN -Help
 proc_help_arguments = ["ue4", "uat", "-utf8output"]
-double_indent = "    "
+DOUBLE_INDENT = "    "
 for line in proc_list.stdout.split('\n'):
-    double_indent_index = line.find(double_indent)
+    double_indent_index = line.find(DOUBLE_INDENT)
     assert double_indent_index == -1 or double_indent_index == 0
     if double_indent_index == 0:
-        proc_help_arguments += [line.replace(double_indent, ""), "-Help"]
+        proc_help_arguments += [line.replace(DOUBLE_INDENT, ''), "-Help"]
 
 # raw output of: RunUAT Command1 -Help Command2 -Help ... CommandN -Help
 proc_help = subprocess.run(proc_help_arguments, capture_output=True, text=True)
@@ -32,10 +33,10 @@ assert proc_help.returncode == 0
 
 # remove unnecessary whitespaces from Help section
 # this is slow but gonna help us with parsing a lot
-proc_help_output = proc_help.stdout.replace("\n     ", " ")
+proc_help_output = proc_help.stdout.replace("\n     ", ' ')
 double_space = "  "
 while proc_help_output.find(double_space) != -1:
-    proc_help_output = proc_help_output.replace(double_space, " ")
+    proc_help_output = proc_help_output.replace(double_space, ' ')
 proc_help_output = proc_help_output.replace("\n -", "\n-")
 
 # From now on we gonna parse proc_help_output
@@ -59,34 +60,24 @@ proc_help_output = proc_help_output.replace("\n -", "\n-")
 
 @dataclasses.dataclass
 class UATParameter:
-    name: str = ""
-    description: str = ""
-    default: str = ""
+    name: str = ''
+    description: str = ''
+    default: str = ''
 
 @dataclasses.dataclass
 class UATCommand:
-    name: str = ""
-    help: str = ""
+    name: str = ''
+    help: str = ''
     parameters: [UATParameter] = dataclasses.field(default_factory=list)
 
 # enumeration - shows where we are while parsing
-START = 0
-HELP = 1
-PARAMS = 2
-END = 3
-
-# common phrases and symbols occuring while parsing:
-TEXT_HELP = " Help:"
-TEXT_PARAMETERS = "Parameters:"
-TEXT_DUPLICATE = "Duplicated help parameter \""
-CHAR_SPACE = " "
-CHAR_DASH = "-"
-CHAR_EQUAL = "="
-CHAR_NEWLINE = "\n"
-CHAR_EMPTY = ""
+STAGE_START = 0
+STAGE_HELP = 1
+STAGE_PARAMS = 2
+STAGE_END = 3
 
 # current state of parser
-current_stage = START
+current_stage = STAGE_START
 current_command: UATCommand or None = None
 pendign_duplicates: {str: UATCommand} = {}
 
@@ -95,42 +86,41 @@ out_commands: [UATCommand] = []
 
 # PARRRRRSSSSSINGGGG...
 # this is slow because we go line by line instead of character by character,
-#   and then we are doing costly substring searching instead of tokenazing
-#   but current aproach is just supper simple
-# btw, if it ever breaks, it's probably easier to re-write it than fixing XD
-for line in proc_help_output.split(CHAR_NEWLINE):
-    if line == CHAR_EMPTY:
+#   and then we are doing costly substring searches instead of tokenazing
+#   but current aproach is just supper simple and quick to implement
+for line in proc_help_output.split('\n'):
+    if line == '':
         pass
-    elif -1 != line.find(TEXT_HELP):
-        current_stage = HELP
+    elif -1 != line.find(" Help:"):
+        current_stage = STAGE_HELP
         if current_command:
             out_commands += [current_command]
-        i_space = line.find(CHAR_SPACE)
+        i_space = line.find(' ')
         assert i_space != -1
         name = line[:i_space]
         current_command = UATCommand(name=name)
-    elif -1 != line.find(TEXT_PARAMETERS):
-        current_stage = PARAMS
-    elif current_stage == START or current_stage == END:
+    elif -1 != line.find("Parameters:"):
+        current_stage = STAGE_PARAMS
+    elif current_stage == STAGE_START or current_stage == STAGE_END:
         pass
-    elif current_stage == HELP:
-        current_command.help += line + CHAR_NEWLINE
-    elif current_stage == PARAMS:
-        if -1 != line.find(TEXT_DUPLICATE):
-            i_dash = line.find(CHAR_DASH)
+    elif current_stage == STAGE_HELP:
+        current_command.help += line + '\n'
+    elif current_stage == STAGE_PARAMS:
+        if -1 != line.find("Duplicated help parameter \""):
+            i_dash = line.find('-')
             param_name = line[i_dash:-1]
             param = UATParameter(name=param_name)
             current_command.parameters += [param]
             pendign_duplicates[param_name] = param
-        elif line[0] == CHAR_DASH:
+        elif line[0] == '-':
             i_dash = 0
-            i_equal = line.find(CHAR_EQUAL)
-            i_space = line.find(CHAR_SPACE)
+            i_equal = line.find('=')
+            i_space = line.find(' ')
             if i_equal > i_space:
                 i_equal = -1
-            param_name = CHAR_EMPTY
-            param_description = CHAR_EMPTY
-            param_default = CHAR_EMPTY
+            param_name = ''
+            param_description = ''
+            param_default = ''
             if i_equal != -1 and i_space != -1:
                 param_name = line[i_dash:i_equal]
                 param_description = line[i_space + 1:]
@@ -156,16 +146,16 @@ for line in proc_help_output.split(CHAR_NEWLINE):
                 pendign_duplicates[param_name].default = param_default
                 pendign_duplicates.pop(param_name)
         else:
-            current_stage = END
+            current_stage = STAGE_END
     else:
         assert False
 
 # out_commands contains parsed data
 
-with open("./out.txt", 'wb') as file:
+with open("./out.txt", "wb") as file:
     pickle.dump(out_commands, file)
 
-with open("./out.txt", 'rb') as file:
+with open("./out.txt", "rb") as file:
     out_commands = pickle.load(file)
 
 print(out_commands)
